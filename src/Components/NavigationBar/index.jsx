3c2e00b0e8198fda3df54e1425fe123884330c45
qwerty:-) <danil.prokhorchuk@incode-it.com>
2022-01-25 09:35:47 +0200

    feat: added container

---
+++ src/Components/NavigationBar/index.jsx
@@ -0,0 +1,28 @@
+import {Link, useLocation} from "react-router-dom";
+
+import ROUTES from "./Routes";
+const routes = ROUTES.PUBLIC_ROUTES;
+
+const NavigationBar = () => {
+    const {pathname} = useLocation();
+
+    const isActiveLink = (path) => {        
+        if(path=== pathname){
+            return 'activeLink'
+        }
+        return 'link'
+    }
+
+    return(
+        <div className="navigation-container">
+            {routes.map(({name,path})=> {
+                return(
+                <Link to={path} key={path} className={isActiveLink(path)}>
+                    {name}
+                </Link>)
+            })}
+        </div>
+    )
+}
+
+export default NavigationBar
